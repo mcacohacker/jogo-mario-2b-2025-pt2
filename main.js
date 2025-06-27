c75b4edacd3859d3660ba4d9329aab9190fe3555
mcacohacker <yasmim.isabeli.costa@escola.pr.gov.br>
2025-06-27 07:46:10 -0300

    .

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
-cons startButtom = document.querySelector("start")
-const gameOverScree = document.querySelect(".game-over")
+const startButtom = document.querySelector("start")
+const gameOverScreen = document.querySelect(".game-over")
+const gameOverScreen = document.querySelrctor(".game-over")
 
 let startGame = false;
 
 const jump = () => {
-    mario.classList.add("jump");
+    gameStarted = true;
 
-pipe.style.animation = "nome";
+    pipe.style.animation = "pipe-animation 1.5s infine linear";
 
 startButton.style.display = "none";
 mario.style.opacity = "1";
 style.opacity = "1";
+nuvem.style.opacity = "1";
+}
+
+
+const jump = () => {
+    mario.classList.add("jump");
+
     setTimeout(() => {
         mario.classList.remove("jump");
     }
@@ -30,11 +38,19 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
         mario.style.animation = "none";
         mario.style.left = `${marioPosition}px`;
 
-        mario.src = "./imagem/"game-over.png";
+        mario.src = "./imagem/game-over.png";
         mario.style.width = "75px";
         mario.style.marginLeft = "50px";
 
         clearInterval(loop);
+        gameOverScreen.style.display = "flex";
+    }else if (pipePosition < 0 && gameStarted) {
+        pipe.style.left = ``;
     }
 },10);
 document.addEventListener("keydown", jump);
+
+
+const restartGame = () => {
+    window.location.reload();
+}
